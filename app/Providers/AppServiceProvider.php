3c53ba54219869efe0971b1a30da26016789f009
Your Name <dlugo@fidu.la>
2026-01-13 17:41:45 -0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         // Forzar HTTPS en producción o cuando APP_URL usa HTTPS
         if ($this->app->environment('production') || str_starts_with(config('app.url'), 'https')) {
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
